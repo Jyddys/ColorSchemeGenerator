b44c03aa11065d4a8af5b17ff3e9f3919b1c796c
jyddys1 <justus.sydanmaa@hotmail.com>
2022-12-06 18:42:22 +0200

    Desing changes

--- script.js
+++ script.js
@@ -1,18 +1,53 @@
 const colorPickValue = document.getElementById('color-pick')
 const colorPickBtn = document.getElementById('get-color')
 const color1 = document.getElementById('color-1')
+
+
 colorPickBtn.addEventListener('click', getColor)
 
 function getColor() {
-console.log(colorPickValue)
-color1.style.backgroundColor = colorPickValue.value
+
+  // Haetaan hex-arvo
+let hexValue = colorPickValue 
+
+let hexArvo = hexValue.value.slice(1,)
+
+ // Haetaan color Scheme arvo
+let themePick = document.getElementById('theme-pick').value
+
+
+// API
+fetch("https://www.thecolorapi.com/scheme?hex=" + hexArvo + "&mode=" + themePick + "&count=5")
+  .then(res => res.json())
+  .then(data => {
+    // const array = data.image.bare
+
+    const hexValue1 = data.colors[0].hex.value
+    const hexValue2 = data.colors[1].hex.value
+    const hexValue3 = data.colors[2].hex.value
+    const hexValue4 = data.colors[3].hex.value
+    const hexValue5 = data.colors[4].hex.value
+
+
+    document.getElementById('hex-1').innerHTML = hexValue1
+    document.getElementById('hex-2').innerHTML = hexValue2
+    document.getElementById('hex-3').innerHTML = hexValue3
+    document.getElementById('hex-4').innerHTML = hexValue4
+    document.getElementById('hex-5').innerHTML = hexValue5
+
+    document.getElementById('color-1').style.backgroundColor = hexValue1
+    document.getElementById('color-2').style.backgroundColor = hexValue2
+    document.getElementById('color-3').style.backgroundColor = hexValue3
+    document.getElementById('color-4').style.backgroundColor = hexValue4
+    document.getElementById('color-5').style.backgroundColor = hexValue5
+
+
+  }
+    )
+
 }
 
 
-fetch("https://www.thecolorapi.com/scheme?hex=F55A5A&mode=monochrome&image&bare")
-  .then(res => res.json())
-  .then(data => {
-    console.log(data[1])
-  
-  }
-    )
+
+
+   
